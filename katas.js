e57d0d26823887730bae4bf2f8fb3a934f103112
Devin B Jackson <32586061+devinbjackson@users.noreply.github.com>
2018-01-26 18:28:46 -0600

    Update katas.js

--- katas.js
+++ katas.js
@@ -114,3 +114,15 @@ function solve(str) {
     return false;
   }
 }
+
+// Positive sum
+
+function positiveSum(arr) {
+  var sum=0;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i]>=0){
+      sum += arr[i]
+    }
+  }
+  return sum
+}
